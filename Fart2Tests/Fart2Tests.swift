c0e48dec02f475d968be0399e993dfee16b436aa
Paul McGrath <gingermcninja@gmail.com>
2025-12-08 15:38:35 -0500

    FART-7 added empty tests

---
+++ Fart2Tests/Fart2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Fart2Tests.swift
+//  Fart2Tests
+//
+//  Created by Paul McGrath on 12/8/25.
+//
+
+import Testing
+
+struct Fart2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
