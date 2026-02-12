475e069291423d6f3901662ff76be347e6453465
Paul McGrath <gingermcninja@gmail.com>
2026-02-11 20:37:39 -0500

    SR-15 Split ContentView into seperate view files

---
+++ SoundRecorder/Views/BoardView.swift
@@ -0,0 +1,94 @@
+//
+//  BoardView.swift
+//  SoundRecorder
+//
+//  Created by Paul McGrath on 2/11/26.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct BoardView: View {
+    func btnPressed(buttonIndex: Int) {
+        var filename: String = "nutz"
+        if buttonIndex != 1 {
+            filename = "fart-0\(buttonIndex)"
+        }
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "wav") else {
+            return
+        }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            guard let player = audioPlayer else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    var body: some View {
+        Color.black.ignoresSafeArea().overlay {
+            VStack {
+                HStack(spacing: 50) {
+                    Button("Button1", action: {
+                        self.btnPressed(buttonIndex: 1)
+                    })
+                    .padding()
+                    .background(Color.red.ignoresSafeArea(.all))
+                    .foregroundStyle(.white)
+                    .clipShape(Capsule())
+                    
+                    Button("Button2", action: {
+                        self.btnPressed(buttonIndex: 2)
+                    }).padding()
+                        .background(Color.red.ignoresSafeArea(.all))
+                        .foregroundStyle(.white)
+                        .clipShape(Capsule())
+
+                }
+                .padding()
+                HStack(spacing: 50) {
+                    Button("Button3", action: {
+                        self.btnPressed(buttonIndex: 3)
+                    }).padding()
+                        .background(Color.red.ignoresSafeArea(.all))
+                        .foregroundStyle(.white)
+                        .clipShape(Capsule())
+
+                    Button("Button4", action: {
+                        self.btnPressed(buttonIndex: 4)
+                    }).padding()
+                        .background(Color.red.ignoresSafeArea(.all))
+                        .foregroundStyle(.white)
+                        .clipShape(Capsule())
+
+                }
+                .padding()
+                HStack(spacing: 50) {
+                    Button("Button5", action: {
+                        self.btnPressed(buttonIndex: 5)
+                    }).padding()
+                        .background(Color.red.ignoresSafeArea(.all))
+                        .foregroundStyle(.white)
+                        .clipShape(Capsule())
+
+                    Button("Button6", action: {
+                        self.btnPressed(buttonIndex: 6)
+                    }).padding()
+                        .background(Color.red.ignoresSafeArea(.all))
+                        .foregroundStyle(.white)
+                        .clipShape(Capsule())
+
+                }
+                .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    BoardView()
+}
